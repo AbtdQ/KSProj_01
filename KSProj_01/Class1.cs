b88864154e7bf7f3db713a2ac48bc5136d4817da
Qxy <Qxy@QL-PC>
2016-11-11 23:43:07 +0800

    Creat Proj

---
+++ KSProj_01/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KSProj_01
+{
+    public class TestModule : PartModule
+    {
+        [KSPField(isPersistant = false, guiActive = true, guiActiveEditor = true, guiName = "Ti"), UI_FloatRange(minValue = 0f, maxValue = 0f, stepIncrement = 1f, scene = UI_Scene.Editor)]
+        public float TestNum;
+
+        public void FixedUpdate()
+        {
+
+        }
+
+        public override void OnStart(StartState state)
+        {
+            var FloatRangeCtrl = (UI_FloatRange) Fields["TestNum"].uiControlEditor;
+            FloatRangeCtrl.maxValue = 2000f;
+            if (state == StartState.Editor || state == StartState.None) return;
+            base.OnStart(state);
+        }
+    }
+}
