fd1c39fe6a7cd683db0a16eb66221f5c2d883591
AbtdQ <abaotiandi1205@outlook.com>
2016-11-12 12:28:21 +0800

    add FixedUpdate()

--- KSProj_01/Class1.cs
+++ KSProj_01/Class1.cs
@@ -9,13 +9,18 @@ namespace KSProj_01
 {
     public class TestModule : PartModule
     {
-        [KSPField(isPersistant = false, guiActive = true, guiActiveEditor = true, guiName = "Ti"), UI_FloatRange(minValue = 0f, maxValue = 0f, stepIncrement = 1f, scene = UI_Scene.Editor)]
+        [KSPField(isPersistant = false, guiActive = true, guiActiveEditor = true, guiName = "Buoyancy"), UI_FloatRange(minValue = 0f, maxValue = 0f, stepIncrement = 1f, scene = UI_Scene.Editor)]
         public float TestNum;
+
+        public void FixedUpdate()
+        {
+            part.buoyancy = TestNum;
+        }
 
         public override void OnStart(StartState state)
         {
             var FloatRangeCtrl = (UI_FloatRange) Fields["TestNum"].uiControlEditor;
-            FloatRangeCtrl.maxValue = 2000f;
+            FloatRangeCtrl.maxValue = 20f;
             if (state == StartState.Editor || state == StartState.None) return;
             base.OnStart(state);
         }
