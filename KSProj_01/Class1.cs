e34e88aad3d337ffbc9b6d88391ce2bfefb4ecbd
AbtdQ <abaotiandi1205@outlook.com>
2016-11-17 23:20:25 +0800

    Fix OnLoad; Fix floatAccProblem; Add ProgressBar;

--- KSProj_01/Class1.cs
+++ KSProj_01/Class1.cs
@@ -1,60 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using UnityEngine;
 
-namespace KSProj_01
-{
-    public class TestModule : PartModule
-    {
-        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Buoyancy")]
-        [UI_FloatRange(minValue = 0f, maxValue = 0f, stepIncrement = 0.05f, affectSymCounterparts = UI_Scene.Editor, scene = UI_Scene.All)]
-        public float CurrentBuoyancy;
+//namespace KSProj_01
+//{
+//    public class TestModule : PartModule
+//    {
+//        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Buoyancy")]
+//        [UI_FloatRange(minValue = 0f, maxValue = 0f, stepIncrement = 0.05f, affectSymCounterparts = UI_Scene.Editor, scene = UI_Scene.All)]
+//        public float CurrentBuoyancy;
 
-        [KSPField(isPersistant = false)]
-        public float DefaultBuoyancy = 1f;
+//        [KSPField(isPersistant = false)]
+//        public float DefaultBuoyancy = 1f;
 
-        //[KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Add Buoyancy")]
-        //public void AddBuoyancy()
-        //{
-        //    if (CurrentBuoyancy < DefaultBuoyancy)
-        //        CurrentBuoyancy += 0.05f;
-        //    CurrentBuoyancy = (float)Math.Round(CurrentBuoyancy, 2);
-        //}
+//        //Use UI_FloatRange Instead
+//        //[KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Add Buoyancy")]
+//        //public void AddBuoyancy()
+//        //{
+//        //    if (CurrentBuoyancy < DefaultBuoyancy)
+//        //        CurrentBuoyancy += 0.05f;
+//        //    CurrentBuoyancy = (float)Math.Round(CurrentBuoyancy, 2);
+//        //}
 
-        //[KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Reduce Buoyancy")]
-        //public void ReduceBuoyancy()
-        //{
-        //    if (CurrentBuoyancy > 0f)
-        //        CurrentBuoyancy -= 0.05f;
-        //    CurrentBuoyancy = (float)Math.Round(CurrentBuoyancy, 2);
-        //}
+//        //[KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Reduce Buoyancy")]
+//        //public void ReduceBuoyancy()
+//        //{
+//        //    if (CurrentBuoyancy > 0f)
+//        //        CurrentBuoyancy -= 0.05f;
+//        //    CurrentBuoyancy = (float)Math.Round(CurrentBuoyancy, 2);
+//        //}
 
-        public void FixedUpdate()
-        {
-            if (HighLogic.LoadedSceneIsFlight)
-            {
-                part.buoyancy = CurrentBuoyancy;
-            }
-        }
+//        public void FixedUpdate()
+//        {
+//            if (HighLogic.LoadedSceneIsFlight)
+//            {
+//                part.buoyancy = CurrentBuoyancy;
+//            }
+//        }
 
-        public override void OnStart(StartState state)
-        {
-            base.OnStart(state);
+//        public override void OnStart(StartState state)
+//        {
+//            base.OnStart(state);
 
-            ((UI_FloatRange)(Fields["CurrentBuoyancy"].uiControlEditor)).maxValue = DefaultBuoyancy;
-            ((UI_FloatRange)(Fields["CurrentBuoyancy"].uiControlFlight)).maxValue = DefaultBuoyancy;
+//            //UI Init in Editor
+//            ((UI_FloatRange)(Fields["CurrentBuoyancy"].uiControlEditor)).maxValue = DefaultBuoyancy;
 
-            switch (state)
-            {
-                case StartState.Editor:
-                    CurrentBuoyancy = DefaultBuoyancy;
-                    break;
-                case StartState.None:
-                    break;
-            }
-        }
-    }
-}
+//            //UI Init in Flight
+//            ((UI_FloatRange)(Fields["CurrentBuoyancy"].uiControlFlight)).maxValue = DefaultBuoyancy;
+//            ((UI_FloatRange)(Fields["CurrentBuoyancy"].uiControlFlight)).controlEnabled = false;
+
+//            switch (state)
+//            {
+//                case StartState.Editor:
+//                    CurrentBuoyancy = DefaultBuoyancy;
+//                    break;
+//                case StartState.None:
+//                    break;
+//            }
+//        }
+//    }
+//}
---
+++ KSProj_01/ModuleBuoyancyCurve.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KSProj_01
+{
+    class ModuleBuoyancyCurve : PartModule
+    {
+        [KSPField(guiName = "Buoyancy", guiActive = true, guiActiveEditor = true)]
+        [UI_ProgressBar(minValue = 0f, maxValue = 0f, scene = UI_Scene.All)]
+        public float currentBuoyancyDisplay;
+
+        public FloatCurve maxtempBuoyancyCurve = new FloatCurve();
+
+        [KSPField(isPersistant = true)]
+        public double currentMaxTemp = float.MinValue;
+
+        public void FixedUpdate()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                if (part.temperature > currentMaxTemp)
+                {
+                    currentMaxTemp = part.temperature;
+
+                    currentBuoyancyDisplay = (float)Math.Round(maxtempBuoyancyCurve.Evaluate((float)currentMaxTemp), 2);
+                    part.buoyancy = currentBuoyancyDisplay;
+                }
+            }
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+
+            if (node.HasNode("maxTempBuoyancyCurve"))
+            {
+                Debug.Log("[BuoyancyCurve]Loading Curve");
+                maxtempBuoyancyCurve.Load(node.GetNode("maxTempBuoyancyCurve"));
+            }
+            else
+            {
+                Debug.Log("[BuoyancyCurve]maxTempBuoyancyCurve is missing, creating default");
+                maxtempBuoyancyCurve.Add(0f, 1f);
+                maxtempBuoyancyCurve.Add(1000f, 0.7f);
+                maxtempBuoyancyCurve.Add(3200f, 0f);
+            }
+            Debug.Log("[BuoyancyCurve]UI init");
+            float tmaxValue, tminValue;
+            maxtempBuoyancyCurve.FindMinMaxValue(out tminValue, out tmaxValue);
+            ((UI_ProgressBar)(Fields["currentBuoyancyDisplay"].uiControlEditor)).minValue = tminValue;
+            ((UI_ProgressBar)(Fields["currentBuoyancyDisplay"].uiControlEditor)).maxValue = tmaxValue;
+            ((UI_ProgressBar)(Fields["currentBuoyancyDisplay"].uiControlFlight)).minValue = tminValue;
+            ((UI_ProgressBar)(Fields["currentBuoyancyDisplay"].uiControlFlight)).maxValue = tmaxValue;
+            currentBuoyancyDisplay = maxtempBuoyancyCurve.Evaluate(298f);
+        }
+    }
+}
